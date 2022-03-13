ef4f3be2d55250b5aa6bf20a4ac3d837c60993f4
Terralien-jp <terralienjp@gmail.com>
2022-03-14 00:54:33 +0900

    first commit

---
+++ type_hints.py
@@ -0,0 +1,12 @@
+price: int = 100
+tax: float = 1.1
+
+def calc_price_incrudeing_tax(price: int, tax: float) -> int:
+    return int(price*tax)
+
+if __name__ == '__main__':
+    print(f'{calc_price_incrudeing_tax(price=price, tax=tax)}円')
+# from typing import List, Dict
+
+# sample_list: List[int] = [1, 2, 3, 4]
+# sample_dict: Dict[str, str] = {'username', 'hoge'}
