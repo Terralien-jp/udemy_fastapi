df8ccc9018bdd4ca2f27b3a46d29503d271c7f10
Terralien-jp <terralienjp@gmail.com>
2022-03-14 16:28:05 +0900

    fix main.py

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # udemy_fastapi
 Python初学者のネクストステップ！FastAPIによるWeb API開発講座
+
+## Webserver(uvicorn)起動コマンド
+```python
+uvicorn main:app --reload
+```
--- main.py
+++ main.py
@@ -1,7 +1,29 @@
 from fastapi import FastAPI
+from typing import Optional
+from pydantic import BaseModel
+from pydantic.types import NonNegativeFloat
+
+class Item(BaseModel):
+    name: str
+    description: Optional[str] = None
+    price: int
+    tax: Optional[float] = None
 
 app = FastAPI()
 
-@app.get("/")
-async def index():
-    return {"message": "Hello Work"}
+@app.post("/item/")
+async def create_item(item: Item):
+    return {"message": f"{item.name}は、税込価格{int(item.price*item.tax)}円です。"}
+
+# @app.get("/")
+# async def index():
+#     return {"message": "Hello Work"}
+
+# パス&クエリパラメータ
+
+# @app.get("/countries/{country_name}")
+# async def country(country_name: str = 'japan', city_name: str ='tokyo'):
+#     return {
+#         "country_name": country_name,
+#         "city_name": city_name
+#     }
